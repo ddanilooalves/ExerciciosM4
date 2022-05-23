a9a472860ee89ba2cf3a4224508789d0f3ad7772
ddanilooalves <daniloalves0512@gmail.com>
2022-05-23 20:03:14 -0300

    add

--- src/table/entities/table.entity.ts
+++ src/table/entities/table.entity.ts
@@ -1,8 +1,12 @@
 interface User {
+      name: String,
+      age: Number,
+      occupation: String
+};
 
-}
+export type Users = unknown;
 
-export const users: User[] = [
+export const users: Users[] = [
     {
         name: "Wilker",
         age: 25,
@@ -18,6 +22,6 @@ export const users: User[] = [
 export function logPerson(user: User) {
     console.log(` - ${user.name}, ${user.age}`);
 }
-
 console.log('Users:');
 users.forEach(logPerson);
+
--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTableDto } from './dto/create-table.dto';
-import { User } from './entities/table.entity';
+import { Users } from './entities/table.entity';
 
 @Injectable()
 export class TableService {
-  users: User[] = [];
+  users: Users[] = [];
 
   findAll() {
     return this.users;
   }
 
   create(createTableDto: CreateTableDto) {
-    const user: User = {
+    const user: Users = {
       ...createTableDto,
       number: 0
     };
